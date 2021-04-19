003d8381912e9e2625483def34ef7dbc08112c8b
Diego Dominguez <d.dominguez.glez1111@gmail.com>
2021-04-19 15:29:03 +0200

    Added message simulation through Scheduler Service

--- HelloFX.java
+++ HelloFX.java
@@ -1,11 +1,19 @@
 import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
 
-import org.graalvm.polyglot.*;
+import javafx.concurrent.ScheduledService;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import org.graalvm.polyglot.*;
 
 public class HelloFX extends Application {
 
@@ -17,21 +25,55 @@ public class HelloFX extends Application {
         Scene scene = new Scene(new StackPane(l), 640, 480);
         stage.setScene(scene);
         stage.show();
+        ScheduledService<String> service = new MessageListener();
+        service.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+            @Override
+            public void handle(WorkerStateEvent workerStateEvent) {
+                if(stage.isIconified())notifyMessage();
+                System.out.println((String) workerStateEvent.getSource().getValue());
+            }
+        });
+        service.start();
     }
 
-    public static void main(String[] args) throws IOException {
-	    Context polyglot = Context.newBuilder()
-                               .allowAllAccess(true)
-                               .build();
-        File file = new File("notifier");
-        Source source = Source.newBuilder("llvm", file).build();
-	    Value cpart = polyglot.eval(source);
-        String name = "Diego";
-        String message = "Hi m8s";
-        
-        if(cpart.canInvokeMember("notify_message")){
-            cpart.invokeMember("notify_message",name, message);
-        };
+    public static void main(String[] args) {
         launch();
+    }
+
+    public void notifyMessage() {
+        try(Context polyglot = Context.newBuilder()
+                               .allowAllAccess(true)
+                               .build())
+        {
+            File file = new File("notifier");
+            Source source = Source.newBuilder("llvm", file).build();
+            Value cpart = polyglot.eval(source);
+            String name = "Diego";
+            String message = "Hi m8s";
+                                
+            if(cpart.canInvokeMember("notify_message")) cpart.invokeMember("notify_message",name, message);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    class MessageListener extends ScheduledService<String> {
+
+        @Override
+        protected Task<String> createTask() {
+            
+            return new Task<String>() {
+                @Override
+                protected String call() throws Exception {
+                    /*
+                    You can make any type of connection in this method without having to handle any type of exception,
+                    if this method throws an exception the service will call it again. I'm just going to pretend that messages are arriving.
+                    */
+                    Thread.sleep(10000);
+                    return "hola";
+                }
+            };
+        }
     }
 }
